7e00f4f137eb79274ad9e0050069f9ba5113d0eb
chaoticgoodangel <pereza4044@gmail.com>
2022-01-11 15:07:43 +0000

    Updated test & page header

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,7 +13,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    assert_select "a[href='https://news.railstutorial.org/']"
+    assert_select "a[href=?]", news_path
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
